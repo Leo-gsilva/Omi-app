07f40baa09fce5f842594d884ac35bf146264337
Igor Carrasco <196232533+BGNS42@users.noreply.github.com>
2026-08-19 14:44:45 -0300

    Mais algumas alterações para utilizar do banco de dados

---
+++ Omi/Omi/View/DetalhesReceitaView.swift
@@ -0,0 +1,18 @@
+//
+//  DetalhesReceitaView.swift
+//  Omi
+//
+//  Created by Igor Carrasco on 19/08/26.
+//
+
+import SwiftUI
+
+struct DetalhesReceitaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetalhesReceitaView()
+}
