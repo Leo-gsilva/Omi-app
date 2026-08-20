7e99532d7094eb1589dc669ffc820be4eef864e4
Guilherme Alves de Souza <guimourast@gmail.com>
2026-08-20 14:10:55 -0300

    terminei a animacao/ falta adicionar as outras paginas

--- Omi/Omi/Componentes/TrocarPagina.swift
+++ Omi/Omi/Componentes/TrocarPagina.swift
@@ -16,47 +16,66 @@ struct TrocarPagina: View {
     let avancar: () -> Void
     
     var body: some View {
+        
         GeometryReader { geo in
+            
             HStack {
-                
+ 
                 Button(action: voltar) {
                     Image(systemName: "arrow.left")
                         .foregroundStyle(.black)
                         .font(.system(size: 25))
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .infinity
+                        )
                 }
-                .frame(width: geo.size.width * 0.35,height: 50)
-                .background(Color.cordaTabBar)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(
+                    width: geo.size.width * 0.30,
+                    height: 50
+                )
+                .contentShape(Rectangle())
+                .glassEffect()
                 
                 Text("\(paginaAtual)/\(totalPaginas)")
-                    .font(.system(size: 20, weight: .semibold))
-                    .frame(width: geo.size.width * 0.20,height: 50)
-                
+                    .font(
+                        .system(
+                            size: 20,
+                            weight: .semibold
+                        )
+                    )
+                    .frame(
+                        width: geo.size.width * 0.20,
+                        height: 50
+                    )
+        
                 Button(action: avancar) {
                     Image(systemName: "arrow.right")
                         .foregroundStyle(.black)
                         .font(.system(size: 25))
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .infinity
+                        )
                 }
-                .frame(width: geo.size.width * 0.35,height: 50)
-                .background(Color.cordaTabBar)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(
+                    width: geo.size.width * 0.30,
+                    height: 50
+                )
+                .contentShape(Rectangle())
+                .glassEffect()
             }
             .frame(maxWidth: .infinity)
         }
     }
 }
 
+
 #Preview {
     TrocarPagina(
-        paginaAtual: 0,
-        totalPaginas: 3,
-        voltar: {
-            print("Voltar")
-        },
-        avancar: {
-            print("Avançar")
-        }
+        paginaAtual: 1,
+        totalPaginas: 5,
+        voltar: {},
+        avancar: {}
     )
 }
